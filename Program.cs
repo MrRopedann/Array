b4b3b8bda6b0dcb5b2958256fab89059e1452713
MrRopedann <bregnev589@gmail.com>
2019-06-16 15:56:42 +0500

    My array C#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seson = new string[12] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декбрь" };
+            for (int i = 0; i <= 11; i++)
+            {
+                Console.WriteLine(seson[i]);
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
